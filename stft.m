ae9cb509bfdf468d407098c515f7f31af4526396
Kyndler <81043581+Kyndler@users.noreply.github.com>
2021-05-15 21:55:58 +0100

    Udpate da stft

--- stft.m
+++ stft.m
@@ -1,24 +1,24 @@
-function sftf(data)
+function stft(data)
     fs=50;
     Ts=1/fs;
     N = numel(data);
     
-    %time = linspace(0,(N-1)*Ts,N);
-    time = 0;
+    t = N*Ts;
     tFrame = 0;
-    tOverlap = 0;
+    tOverlap = 0.050;
     
     nFrame = round(tFrame*fs);
     nOverlap = round(tOverlap*fs);
     
     h = hamming(Nframe);
     
-    if mod(N,2) == 0
-        f = -Fs/2:Fs/N:Fs/2-Fs/N;
+    if(mod(nFrame,2))==0
+       f = -fs/2:fs/nFrame:fs/2-fs/nFrame;
     else
-        f = -Fs/2+(Fs/N/2):Fs/N:Fs/2-(Fs/N/2);
+        f = -fs/2+fs/(2*nFrame):fs/nFrame:fs/2-fs/(2*nFrame);
     end
     
+    x = find(f>=0);
     fin = [];
     for ii = 1:nFrame-nOverlap:N-nFrame+1
         dFrame = data(ii:ii+nFrame-1).*h;
@@ -29,5 +29,5 @@ function sftf(data)
     figure('Name','STFT ',' NumberTitle', 'off');
     waterfall(20*log10(fin))
     figure('Name', 'STFT', 'NumberTitle','off');
-    imagesec(20*log10(fin))
+    imagesec(20*log10(fin))    
 end
